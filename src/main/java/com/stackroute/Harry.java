b0e880766ab700c36bbd1453161c5e83bf3a5bf8
nilamaj1 <nilanjanmaji08@gmail.com>
2019-01-02 12:14:15 +0530

    final commit

--- src/main/java/com/stackroute/Harry.java
+++ src/main/java/com/stackroute/Harry.java
@@ -1,8 +1,16 @@
 package com.stackroute;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Harry {
-    public String isHarry(String s)
-    {
-        return null;
+    public String isHarry(String queryString) {        Pattern pattern = Pattern.compile("(?:^|\\W)Harry(?:$|\\W)");
+        Matcher matcher = pattern.matcher(queryString);        boolean found = false;
+        while (matcher.find()){
+            found = true;
+        }
+        if(found == true)
+            return "Is Harry here ? True";
+        return "Is Harry here ? False";
     }
 }
